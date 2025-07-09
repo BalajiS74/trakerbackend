ed4bcbda00cff3a872387ae93a2b97d7ffd86d08
BalajiS74 <balaji.sornavel7418@gmail.com>
2025-07-10 02:17:56 +0530

    add-vercel-file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 const app = express();
@@ -9,11 +9,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', require('./routes/auth'));
-
-mongoose.connect(process.env.MONGO_URI)
+app.use("/api", require("./routes/auth"));
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('MongoDB Connected');
-    app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+    console.log("MongoDB Connected");
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running on port ${process.env.PORT}`)
+    );
   })
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err));
